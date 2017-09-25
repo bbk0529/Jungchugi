8a4690af8ee9b2176990efffeb174e0fcc2d3d56
bbk0529 <bbk0529@gmail.com>
2017-09-25 12:12:44 +0900

    Add files via upload

---
+++ jung.py
@@ -0,0 +1,21 @@
+﻿import numpy as np
+fr = open('test.txt')
+#fr = open('test.txt', encoding='utf-8')
+problem=[]
+tempLine=''
+
+for currLine in fr.readlines() :
+	if currLine.find('*')!=-1 :
+		if tempLine !='' : 
+			problem.append(tempLine.split(':'))
+		tempLine = currLine
+	else : 
+		tempLine +=currLine
+
+while True:
+	n=np.random.randint(0,len(problem)-1)
+	print('================================================')
+	print(problem[n][1])
+	a=input()
+	if a=='q' : break
+	print(problem[n][0] + '\n\n')
